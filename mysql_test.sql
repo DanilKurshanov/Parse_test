c708b3791953ef78c546e0d58928ecea78cce5e8
DanilKurshanov <66821904+DanilKurshanov@users.noreply.github.com>
2022-10-25 13:42:52 +0300

    Add files via upload
    SQL query

---
+++ mysql_test.sql
@@ -0,0 +1,33 @@
+SELECT 
+producer_name, product_img_url,
+product_url, product_price_min, 
+product_short_description
+FROM grommet_products
+JOIN grommet_product_categories ON grommet_products.id = grommet_product_categories.product_id
+JOIN grommet_gifts_categories ON grommet_product_categories.product_category_id = grommet_gifts_categories.id
+WHERE grommet_gifts_categories.sub_category = 'Jewelry' AND grommet_products.is_sold_out = False;
+
+
+SELECT 
+producer_name, product_img_url,
+product_url, product_price_min, 
+product_short_description
+FROM grommet_products
+JOIN grommet_product_to_keyword ON grommet_products.id = grommet_product_to_keyword.product_id
+JOIN grommet_product_keywords ON grommet_product_to_keyword.keyword_id = grommet_product_keywords.id
+WHERE grommet_product_keywords.keyword = 'Hair accessor' AND grommet_products.is_sold_out = False;
+
+
+SELECT 
+producer_name, product_img_url,
+product_url, product_price_min, 
+product_short_description
+FROM grommet_products
+JOIN grommet_product_to_keyword ON grommet_products.id = grommet_product_to_keyword.product_id
+JOIN grommet_product_keywords ON grommet_product_to_keyword.keyword_id = grommet_product_keywords.id
+JOIN grommet_product_categories ON grommet_products.id = grommet_product_categories.product_id
+JOIN grommet_gifts_categories ON grommet_product_categories.product_category_id = grommet_gifts_categories.id
+WHERE 
+(grommet_gifts_categories.sub_category = 'Beauty & Personal Care' OR grommet_gifts_categories.sub_category = 'Skincare') 
+OR grommet_product_keywords.keyword = 'Aromatherapy' 
+AND grommet_products.is_sold_out = False;
